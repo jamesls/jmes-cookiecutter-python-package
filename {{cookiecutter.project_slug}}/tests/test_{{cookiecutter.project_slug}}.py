607ec77adcddbb69c8299f929df1582005b28502
James Saryerwinnie <js@jamesls.com>
2018-04-28 08:14:51 -0700

    Fix import statement

--- {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -1,6 +1,6 @@
 import unittest
 
-from {{ cookiecutter.project_slug }} import {{ cookiecutter.project_slug }}
+import {{ cookiecutter.project_slug }}
 
 
 class Test{{ cookiecutter.project_slug|capitalize }}(unittest.TestCase):
